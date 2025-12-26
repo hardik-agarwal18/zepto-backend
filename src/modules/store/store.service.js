aabe0bb2cd6fcd1089d83054ddb16c425650625d
Hardik Agarwal <hardikagarwal9308@gmail.com>
2025-12-27 03:45:02 +0530

    feat: add store service with create, list, get, and update status functions

---
+++ src/modules/store/store.service.js
@@ -0,0 +1,45 @@
+import { v4 as uuid } from "uuid";
+import db from "../../config/db.js";
+import * as storeRepo from "./store.repo.js";
+
+export async function createStore(data) {
+  const client = await db.getClient();
+  try {
+    await client.query("BEGIN");
+
+    const store = await storeRepo.createStore(client, {
+      id: uuid(),
+      ...data,
+    });
+
+    await client.query("COMMIT");
+    return store;
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
+}
+
+export async function listStores() {
+  return storeRepo.listActiveStores();
+}
+
+export async function getStore(id) {
+  return storeRepo.findById(id);
+}
+
+export async function setStoreStatus(id, isActive) {
+  const client = await db.getClient();
+  try {
+    await client.query("BEGIN");
+    await storeRepo.updateStatus(client, id, isActive);
+    await client.query("COMMIT");
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
+}
